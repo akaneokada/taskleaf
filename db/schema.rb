f37af27929339af2d9fba3cddcee4ad4bee08ed3
aknokd <aknokd.814@gmail.com>
2020-10-01 15:46:50 +0900

    tasksテーブルのnameカラムにNOT NULL制約をつける

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_094919) do
+ActiveRecord::Schema.define(version: 2020_10_01_064454) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
