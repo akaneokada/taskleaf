615979717a00b2566b6e4e289b52b07085309f7a
aknokd <aknokd.814@gmail.com>
2020-10-03 20:20:43 +0900

    一覧表示機能のテスト

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "タスク管理機能", type: :system do
+  describe '一覧表示機能' do
+    # letでオブジェクトを定義する
+    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+    
+    before do
+      # 作成者がユーザーAであるタスクを作成しておく
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+      # ログインの処理をまとめる
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
+    end
+
+    context 'ユーザーAがログインしているとき' do
+      # ユーザーAでログインする
+      let(:login_user) { user_a }
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        # 作成ずみのタスクの名称が画面に表示されていることを確認
+        expect(page).to have_content '最初のタスク'
+      end
+    end
+
+    context 'ユーザーBがログインしているとき' do
+      # ユーザーBでログインする
+      let(:login_user) { user_b }
+
+      it 'ユーザーAが作成したタスクが表示されない' do
+        # ユーザーAが作成したタスクの名称が画面に表示されていないことを確認
+        expect(page).to have_no_content '最初のタスク'
+      end
+    end
+  end
+end
